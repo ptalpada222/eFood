40cc3ead48497190d0ce0380828d22aa486b4ef9
Kenil855 <kenil8236@gmail.com>
2026-02-05 23:31:57 +0530

    Add Hero and Servises jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Footer from "./Components/footer.jsx";
 import About from "./Components/about.jsx";
 import Navbar from "./Components/navbar.jsx";
+import HeroSlider from "./Components/Hero.jsx";
+import Services from "./Components/Services.jsx";
 
 function App() {
   return (
     <>
       <div className="">
         <Navbar />
+        <HeroSlider/>
+        <Services/>
         <About />
         <Footer />
       </div>
